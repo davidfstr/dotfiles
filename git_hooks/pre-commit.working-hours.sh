bb508b68ee7e8599f47a8deb23af492491c798a2
David Foster <david@dafoster.net>
2023-03-03 08:59:37 -0500

    pre-commit.working-hours.sh: Alter working hours. Fix to be timezone aware.

--- git_hooks/pre-commit.working-hours.sh
+++ git_hooks/pre-commit.working-hours.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-WEEKDAY_ORDINAL=`date +"%u"`  # 1=Mon, 6=Sat, 7=Sun
-TIME=`date +"%H:%M"`
-if [[ "$WEEKDAY_ORDINAL" < "6" ]] && [[ "08:00" < "$TIME" ]] && [[ "$TIME" < "17:00" ]]; then
+WORK_TZ=America/Los_Angeles  # Pacific Time
+WEEKDAY_ORDINAL=$(TZ=$WORK_TZ date +"%u")  # 1=Mon, 6=Sat, 7=Sun
+TIME=$(TZ=$WORK_TZ date +"%H:%M")
+if [[ "$WEEKDAY_ORDINAL" < "6" ]] && [[ "07:00" < "$TIME" ]] && [[ "$TIME" < "15:00" ]]; then
     echo "ERROR: Avoid contributing to open source during working hours. Consider:" >&2
     echo "(1) throwaway this commit and rewrite it offhours," >&2
     echo "(2) amend the commit to a correct time that is offhours," >&2
